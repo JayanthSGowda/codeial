0faf7b3d986451d28c964e32b6e1ca560a710e2f
luffy <jayanth.gowda2807@gmail.com>
2023-07-28 01:16:54 +0530

    user authentication page structure form

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,3 +4,26 @@ module.exports.profile = function(req, res){
         user_name: "jayanth",
     })
 }
+//render user sign up
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title:'codeial | Sign Up'
+    })
+}
+
+//render user sign up
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title:'codeial | Sign In'
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    // todo later
+}
+
+// Sing in and create a session for the user
+module.exports.createSession = function(req, res){
+    // todo later
+}
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user_controller');
-router.get('/', userController.profile);
+router.get('/profile', userController.profile);
+router.get('/sign-up', userController.signUp);
+router.get('/sign-in', userController.signIn);
+
 
 module.exports = router;

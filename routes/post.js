050760c3e3c3c49c61776377b9035f6501e9db11
luffy <jayanth.gowda2807@gmail.com>
2023-08-12 11:27:30 +0530

    added authentication posts

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 
 const postController = require('../controllers/post_controller');
-router.post('/create-post', postController.createPost);
+router.post('/create-post',passport.checkAuthentication, postController.createPost);
 
 
 module.exports = router;
